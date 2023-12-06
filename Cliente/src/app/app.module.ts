601c883a24507d44c188f19f9f07ae59c6cac299
kiri-1 <belenjesuslp@gmail.com>
2023-12-06 15:53:49 -0300

    front terminado

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Cliente/src/app/components/crear-producto/crear-producto.component.ts
+++ Cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -9,7 +14,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CrearProductoComponent implements OnInit {
   productoForm:FormGroup; 
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder,
+    private router:Router,
+    private toastr: ToastrService){
     this.productoForm=this.fb.group({
       producto:['', Validators.required],
       categoria:['', Validators.required],
@@ -17,14 +24,23 @@ export class CrearProductoComponent implements OnInit {
       precio:['', Validators.required],
     })
   }
-  
+
   ngOnInit() {
     
   }
 
-
   agregarProducto(){
-    console.log(this.productoForm)
+    
+    const PRODUCTO:Producto={
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+    }
+
+    console.log(PRODUCTO);
+    this.toastr.info('El producto fue registrado con exito','Producto registrado!');
+    this.router.navigate(['/']);
   }
 }
 
